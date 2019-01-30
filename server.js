73d4ff3f8e30fc386c18cbc255b3fdeeff326fa7
Thomas <thomaslee9474@gmail.com>
2019-01-30 14:25:06 -0800

    [update] port

--- server.js
+++ server.js
@@ -24,6 +24,5 @@ app.get('/trainer-api/:trainerName', (req, res) => {
 });
 
 
-app.listen(3000, function() {
-    console.log('App is listening on port 3000')
-})
+const port = process.env.PORT || 3000;
+app.listen(port)
